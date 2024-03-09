36cda621a7c583e8ba357faa8d418b364e0f43c1
ItayElf <thh123321@gmail.com>
2024-03-09 17:27:50 +0200

    Fix: separated card views

--- lib/widgets/card_widget.dart
+++ lib/widgets/card_widget.dart
@@ -9,55 +9,77 @@ class CardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (cardData.imageUrl != null) {
-      return Padding(
-        padding: const EdgeInsets.all(32),
-        child: Center(
+      return _ImageCard(cardData: cardData);
+    }
+
+    return _TextCard(cardData: cardData);
+  }
+}
+
+class _ImageCard extends StatelessWidget {
+  const _ImageCard({super.key, required this.cardData});
+
+  final CardData cardData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width - 40,
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(12)),
+            image: DecorationImage(
+              image: NetworkImage(cardData.imageUrl!),
+              fit: BoxFit.cover,
+            ),
+          ),
           child: Container(
-            width: MediaQuery.of(context).size.width - 40,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(12)),
-              image: DecorationImage(
-                image: NetworkImage(cardData.imageUrl!),
-                fit: BoxFit.cover,
+            padding: const EdgeInsets.all(16),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+              gradient: LinearGradient(
+                colors: [Colors.transparent, Colors.black],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.7, 1],
               ),
             ),
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(12)),
-                gradient: LinearGradient(
-                  colors: [Colors.transparent, Colors.black],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  stops: [0.7, 1],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FittedBox(
+                  child: Text(
+                    cardData.name,
+                    style: const TextStyle(fontSize: 40, color: Colors.white),
+                  ),
                 ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  FittedBox(
-                    child: Text(
-                      cardData.name,
-                      style: const TextStyle(fontSize: 40, color: Colors.white),
+                if (cardData.description != null)
+                  Text(
+                    cardData.description!,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white70,
                     ),
                   ),
-                  if (cardData.description != null)
-                    Text(
-                      cardData.description!,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        color: Colors.white70,
-                      ),
-                    ),
-                ],
-              ),
+              ],
             ),
           ),
         ),
-      );
-    }
+      ),
+    );
+  }
+}
 
+class _TextCard extends StatelessWidget {
+  const _TextCard({super.key, required this.cardData});
+
+  final CardData cardData;
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(32),
       child: Center(
